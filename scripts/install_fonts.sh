e21c6ae1ea37d9ba9946b1ebc9d7ab5e714d1bc8
K14aNB <kirnb@protonmail.com>
2024-11-05 20:20:39 +0530

    Fixed clone destination directory to be ~ instead of current directory

--- scripts/install_fonts.sh
+++ scripts/install_fonts.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Install Roboto Mono Fonts from Google Fonts Repo
-git clone git@github.com:googlefonts/RobotoMono.git
+git clone git@github.com:googlefonts/RobotoMono.git ~/
 
-mkdir -p ~/.local/share/fonts && cp ./RobotoMono/fonts/ttf/*.ttf ~/.local/share/fonts
+mkdir -p ~/.local/share/fonts && cp ~/RobotoMono/fonts/ttf/*.ttf ~/.local/share/fonts
 
 # Install Open Sans Fonts from Google Fonts Repo
-git clone git@github.com:googlefonts/opensans.git
+git clone git@github.com:googlefonts/opensans.git ~/
 
-cp ./opensans/fonts/ttf/*.ttf ~/.local/share/fonts
+cp ~/opensans/fonts/ttf/*.ttf ~/.local/share/fonts
 
 # Refresh Fonts cache
 if sudo fc-cache -f -v | tail -n 3 | grep -q "fc-cache: succeeded"; then
